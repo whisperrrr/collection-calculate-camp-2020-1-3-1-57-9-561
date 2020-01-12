ad5d3df386518f160fa830897b95aad82baa08d3
JinHui <jh731448520@163.com>
2020-01-13 00:37:47 +0800

    [JinHui]{feat}-{#collectionOperator}:finish 1.3/1.4 get_integer_interval/2

--- main/collectionOperator/get_integer_interval.js
+++ main/collectionOperator/get_integer_interval.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function get_integer_interval(number_a, number_b) {
-  //在这里写入代码
-}
+    let interval = number_a - number_b > 0 ? -1 : 1;
+    let result = [];
+    while (number_a !== number_b) {
+      result.push(number_a);
+      number_a += interval;
+    }
+    result.push(number_b);
+    return result;
+  }
+  
+
 
 module.exports = get_integer_interval;
 
--- main/collectionOperator/get_integer_interval_2.js
+++ main/collectionOperator/get_integer_interval_2.js
@@ -1,7 +1,8 @@
 'use strict';
-
+var get_integer_interval = require('../../main/collectionOperator/get_integer_interval.js');
+var collect_all_even = require('../../main/collectionOperator/collect_all_even.js');
 function get_integer_interval_2(number_a, number_b) {
-  //在这里写入代码
+  return collect_all_even(get_integer_interval(number_a,number_b));
 }
 
 module.exports = get_integer_interval_2;
