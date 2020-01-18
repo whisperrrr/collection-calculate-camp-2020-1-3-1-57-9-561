4844af09e48a27512cd3976a1a2297f00d5e3015
JinHui <jh731448520@163.com>
2020-01-18 20:47:21 +0800

    [JinHui]{feat}-{#filter}:finish 2.4 grouping_count

--- main/filter/grouping_count.js
+++ main/filter/grouping_count.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function grouping_count(collection) {
-
-  //在这里写入代码
+  return collection.reduce((acc,cur) => {
+    (cur in acc)? acc[cur] += 1: acc[cur] = 1;
+    return acc;
+  },{})
 }
 
 module.exports = grouping_count;
