0e9dd55aff1d806a6a88e26db843f15498c301cd
JinHui <jh731448520@163.com>
2020-01-18 20:57:22 +0800

    [JinHui]{feat}-{#filter}:finish 2.5 two_collection_practice_1

--- main/filter/two_collections_practice_1.js
+++ main/filter/two_collections_practice_1.js
@@ -1,8 +1,7 @@
 'use strict';
 
 function choose_common_elements(collection_a, collection_b) {
-
-  //在这里写入代码
+  return collection_a.filter(ele => collection_b.indexOf(ele) > -1);
 }
 
 module.exports = choose_common_elements;
