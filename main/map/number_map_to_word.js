430e1d4e7c2a80855ee35d8665d65e0f7bb98767
JinHui <jh731448520@163.com>
2020-01-19 22:16:29 +0800

    [JinHui]{feat}-{#map}:finish 4.4/4.5

--- main/map/number_map_to_word.js
+++ main/map/number_map_to_word.js
@@ -1,6 +1,14 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  return collection.map(ele => numToLetter(ele));
 };
-
+function numToLetter(num) {
+  if (num <= 26) {
+    return String.fromCharCode(96 + num);
+  }
+  else {
+    let letterNow = String.fromCharCode(97 + (num - 1) % 26);
+    return numToLetter(parseInt((num - 1) / 26)) + letterNow;
+  }
+}
 module.exports = number_map_to_word;
--- main/map/number_map_to_word_over_26.js
+++ main/map/number_map_to_word_over_26.js
@@ -1,6 +1,7 @@
 'use strict';
+var number_map_to_word = require('../../main/map/number_map_to_word.js');
 var number_map_to_word_over_26 = function(collection){
-  return ['a','m','aa','ad','y','aa'];
+  return number_map_to_word(collection);
 };
 
 module.exports = number_map_to_word_over_26;
