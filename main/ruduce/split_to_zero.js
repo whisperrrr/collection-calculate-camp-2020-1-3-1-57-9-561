8526c9d8d04f68c2d1499de999ccf9caf500e2e7
JinHui <jh731448520@163.com>
2020-01-19 12:38:25 +0800

    [JinHui]{feat}-{#ruduce}:finish ruduce all

--- main/ruduce/split_to_zero.js
+++ main/ruduce/split_to_zero.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function spilt_to_zero(number, interval) {
-  //在这里写入代码
+  let result = [number];
+  while (number > 0) {
+    number = Math.round((number - interval) * 10) / 10;
+    result.push(number);
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
