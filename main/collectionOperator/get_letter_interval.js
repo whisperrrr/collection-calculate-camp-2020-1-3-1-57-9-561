09b0f74227a955d43a090f2853e4be8d8d609ea5
JinHui <jh731448520@163.com>
2020-01-20 14:38:01 +0800

    [JinHui]{fix}-{#super}:fix average_uneven

--- main/collectionOperator/get_letter_interval.js
+++ main/collectionOperator/get_letter_interval.js
@@ -1,7 +1,9 @@
 'use strict';
+var get_integer_interval = require('../../main/collectionOperator/get_integer_interval.js');
+var number_map_to_word = require('../../main/map/number_map_to_word.js');
 
 function get_letter_interval(number_a, number_b) {
-  //在这里写入代码
+  return number_map_to_word(get_integer_interval(number_a,number_b));
 }
 
 module.exports = get_letter_interval;
--- main/superposition_operation/interval_operation/average_uneven.js
+++ main/superposition_operation/interval_operation/average_uneven.js
@@ -1,9 +1,12 @@
 'use strict';
-var amount_even = require("../../../main/superposition_operation/interval_operation/amount_even.js");
-var compute_elements_sum = require('../../../main/reduce/compute_sum.js');
+
+let compute_elements_sum = require('../../../main/reduce/compute_sum.js');
 
 function average_uneven(collection) {
-  return compute_elements_sum(collection) - amount_even(collection);
+  return compute_elements_sum(choose_uneven(collection))/choose_uneven(collection).length;
+}
+function choose_uneven(collection) {
+  return collection.filter(ele => ele % 2 !== 0);
 }
 
 module.exports = average_uneven;
